b3386754f5c1629f20043da3e32e66aa33164a92
luis.cuesta <luis.cuesta@advantic.es>
2023-07-06 14:23:10 +0200

    feat: Backend y aplicacion login

---
+++ teslo-app/lib/features/auth/domain/datasources/auth_datasource.dart
@@ -0,0 +1,6 @@
+abstrac class AuthDataSource{
+  Future<User> login(String email, String password);
+  Future<User> register(String email, String password);
+  Future<User> checkAuthStatus(String token);
+
+}
